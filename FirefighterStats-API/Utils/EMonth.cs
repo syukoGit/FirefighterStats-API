cbe906f3152b47efd1d9e1de6ff10927d146b4c2
syukoGit <syukodev@outlook.com>
2022-07-01 17:53:39 +0200

    Add PaySlip and PaySlipLine entities

---
+++ FirefighterStats-API/Utils/EMonth.cs
@@ -0,0 +1,37 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright project="FirefighterStats-API" file="EMonth.cs" company="syuko">
+//  Copyright (c) syuko. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace FirefighterStats.Utils;
+
+using JetBrains.Annotations;
+
+[PublicAPI]
+public enum EMonth
+{
+    January,
+
+    February,
+
+    March,
+
+    April,
+
+    May,
+
+    June,
+
+    July,
+
+    August,
+
+    September,
+
+    October,
+
+    November,
+
+    December,
+}
